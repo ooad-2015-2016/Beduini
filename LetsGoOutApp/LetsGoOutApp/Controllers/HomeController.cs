d6cdde86ec2da6bbd5d9d8eb97744820b7a5bc3d
Sanil Musić <music.sanil@gmail.com>
2016-05-23 12:56:59 +0200

    Na početnoj se prikazuju 3 najskorija događaja

--- LetsGoOutApp/LetsGoOutApp/Controllers/HomeController.cs
+++ LetsGoOutApp/LetsGoOutApp/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LetsGoOutApp.Models;
 
 namespace LetsGoOutApp.Controllers
 {
     public class HomeController : Controller
     {
+        private DogadjajDbContext db = new DogadjajDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Dogadjaj.OrderBy(x => x.datum).Take(3).ToList());
         }
 
         public ActionResult About()
