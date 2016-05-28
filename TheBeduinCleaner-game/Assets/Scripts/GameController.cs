54b2e8b6d5376dc3916a0eccef28484245c41001
Mirza Ohranovic <mirza.ohranovic@gmail.com>
2016-05-28 23:26:26 +0200

    k

---
+++ TheBeduinCleaner-game/Assets/Scripts/GameController.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private float maxWidth;
+    public Camera cam;
+
+    public GameObject sto;
+	public GameObject sto_sivi;
+	public GameObject sto_crveni;
+	public GameObject sto_bonus;
+	public GameObject sendvic;
+	public GameObject tanjir;
+	public GameObject kupac;
+
+    public GameObject gameOverBtn;
+    public GameObject gameOverTxt;
+    public GameObject naslov;
+    public GameObject startBtn;
+
+    public float timeLeft = 0.0f;
+    public Text timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float stoWidth = sto.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - stoWidth;
+        
+    }
+
+    public void StartGame()
+    {
+		timeLeft = 30;
+
+        naslov.SetActive(false);
+        startBtn.SetActive(false);
+
+        StartCoroutine(Spawn());
+		StartCoroutine(SpawnSivi());
+		StartCoroutine(SpawnCrveni());
+		StartCoroutine(SpawnBonus());
+		StartCoroutine(SpawnSendvic());
+		StartCoroutine(SpawnTanjir());
+		StartCoroutine(SpawnKupac());
+    }
+
+    void FixedUpdate()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0) timeLeft = 0.0f;
+        timer.text = "Preostalo vremena:\n" + Mathf.RoundToInt(timeLeft).ToString();
+        
+    }
+
+	IEnumerator SpawnConstruct(GameObject ob, float range_start, float range_end){
+
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(ob, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(range_start, range_end));
+        }
+		
+	}
+
+    IEnumerator Spawn()
+    {
+        
+        yield return new WaitForSeconds(2.0f);
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(sto, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        }
+
+        yield return new WaitForSeconds(2);
+        gameOverTxt.SetActive(true);
+        yield return new WaitForSeconds(1);
+        gameOverBtn.SetActive(true);
+    }
+
+	IEnumerator SpawnSivi() {
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(sto_sivi, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(2.0f, 4.0f));
+        }
+    }
+
+    IEnumerator SpawnCrveni()
+    {
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(sto_crveni, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(3.0f, 8.0f));
+        }
+    }
+
+    IEnumerator SpawnBonus()
+    {
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(sto_bonus, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(10.0f, 30.0f));
+        }
+    }
+
+    IEnumerator SpawnSendvic()
+    {
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(sendvic, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(5.0f, 15.0f));
+        }
+    }
+
+    IEnumerator SpawnTanjir()
+    {
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(tanjir, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(0.6f, 8.0f));
+        }
+    }
+
+    IEnumerator SpawnKupac()
+    {
+        while (timeLeft > 0)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(kupac, spawnPos, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(2.0f, 5.0f));
+        }
+    }
+
+
+    
+
+}
---
+++ TheBeduinCleaner-game/Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    public int crniStoVal = 1;
+
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        UpdateScore();
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "crni_sto(Clone)") score += 1;
+        if (other.gameObject.name == "crveni_sto(Clone)") score -= 5;
+        if (other.gameObject.name == "sivi_sto(Clone)") score += 2;
+        if (other.gameObject.name == "bonus_sto(Clone)") score += 10;
+        if (other.gameObject.name == "kupac_box(Clone)") score -= 3;
+        if (other.gameObject.name == "sendvic_box(Clone)") score += 3;
+        if (other.gameObject.name == "tanjir(Clone)") score += 20;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
