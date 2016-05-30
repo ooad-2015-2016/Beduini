1496dc98a8f1b724e5c53cf5d22926680b58c072
Sanil Musić <music.sanil@gmail.com>
2016-05-30 14:16:25 +0200

    Pobrisane suvišne metode

--- LetsGoOutApp/LetsGoOutApp/Controllers/KomentariController.cs
+++ LetsGoOutApp/LetsGoOutApp/Controllers/KomentariController.cs
@@ -14,33 +14,6 @@ namespace LetsGoOutApp.Controllers
     {
         private LetsGoOutAppContext db = new LetsGoOutAppContext();
 
-        // GET: Komentari
-        public ActionResult Index()
-        {
-            return View(db.Komentari.ToList());
-        }
-
-        // GET: Komentari/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Komentar komentar = db.Komentari.Find(id);
-            if (komentar == null)
-            {
-                return HttpNotFound();
-            }
-            return View(komentar);
-        }
-
-        // GET: Komentari/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
         // POST: Komentari/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -55,63 +28,6 @@ namespace LetsGoOutApp.Controllers
             }
 
             return RedirectToAction("Details", "Lokali", new { id = komentar.LokalID });
-        }
-
-        // GET: Komentari/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Komentar komentar = db.Komentari.Find(id);
-            if (komentar == null)
-            {
-                return HttpNotFound();
-            }
-            return View(komentar);
-        }
-
-        // POST: Komentari/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,datum,sadrzaj,ocjena,datumKreiranja")] Komentar komentar)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(komentar).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(komentar);
-        }
-
-        // GET: Komentari/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Komentar komentar = db.Komentari.Find(id);
-            if (komentar == null)
-            {
-                return HttpNotFound();
-            }
-            return View(komentar);
-        }
-
-        // POST: Komentari/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Komentar komentar = db.Komentari.Find(id);
-            db.Komentari.Remove(komentar);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
